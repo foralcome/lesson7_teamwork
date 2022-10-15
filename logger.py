8a0f21f7803c0c2b2d22c9f1987605251a0ded05
a.dergachev <bestacc@mail.ru>
2022-10-15 12:13:35 +0300

    edit logger module

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 from datetime import datetime as dt
 
 
-def operation_logger(data_typ, op_type, inp_data, res):
+def operation_logger(data_typ: str, op_type: str, inp_data: list, res: int):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
         file.write('{};data type;{};operation type;{};input data;{};result;{}'
