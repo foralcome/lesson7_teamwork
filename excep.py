e7eb7a9622df8440ead7da7d15c5b1e3d6b9cb38
a.dergachev <bestacc@mail.ru>
2022-10-15 21:02:09 +0300

    create exception module

---
+++ excep.py
@@ -0,0 +1,10 @@
+def check_inp_data(val: str):
+    if val.isdigit() or val[0] == '-' and val[1:].isdigit() \
+            or val[0].isdigit() and val[1] == '.' and val[2:].isdigit() \
+            or val[0] == '-' and val[1].isdigit() and val[2] == '.' and val[3:].isdigit():
+        return val
+    else:
+        print(f"Error, {val} is not an appropriate argument! Pleas try again.")
+
+
+check_inp_data(input("Enter: "))
