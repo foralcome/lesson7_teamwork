dbb676ed41a060f39ef224876be63bac5e7b7d79
a.dergachev <bestacc@mail.ru>
2022-10-15 21:03:27 +0300

    edit logger module

--- logger.py
+++ logger.py
@@ -1,10 +1,8 @@
 from datetime import datetime as dt
-# необходимо собрать данные из интерфейса
-# import val_1, op_type, val_2, res
 
 
-def operation_logger(val_1, op_type, val_2, res):
+def operation_logger(data_typ: str, op_type: str, inp_data: list, res: int):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{};{};{};{}; = ;{}'
-                   .format(time, val_1, op_type, val_2, res))
+        file.write('{};data type;{};operation type;{};input data;{};result;{}'
+                   .format(time, data_typ, op_type, inp_data, res))
